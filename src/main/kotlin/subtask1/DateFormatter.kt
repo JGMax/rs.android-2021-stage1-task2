d58da906a34db3b543628357e742907440dbe503
JGMax <maksus20.05@mail.ru>
2021-06-11 17:22:02 +0300

    fix: fix date formatter

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,26 +1,18 @@
 package subtask1
 
 import java.text.ParseException
-import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import java.util.*
 
 class DateFormatter {
-
     fun toTextDay(day: String, month: String, year: String): String {
-        val toDateParser = SimpleDateFormat("yyyy-MM-dd", Locale("ru", "RU"))
-        toDateParser.isLenient = false
+        val formatter = DateTimeFormatter.ofPattern("d MMMM, EEEE", Locale("ru"))
+        val date = LocalDate.of(year.toInt(), month.toInt(), day.toInt())
         return try {
-            val date = toDateParser.parse("$year-${month.dateFormat()}-${day.dateFormat()}")
-            SimpleDateFormat("d MMMM, EEEE", Locale("ru", "RU")).format(date)
+            date.format(formatter)
         } catch (e: ParseException) {
             "Такого дня не существует"
         }
     }
-
-    private fun String.dateFormat() =
-        if (toInt() in 0 until 10) {
-            "0$this"
-        } else {
-            this
-        }
 }
