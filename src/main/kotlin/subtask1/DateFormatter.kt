15325db33e3b67216d887bd2b574ade808526f1a
JGMax <maksus20.05@mail.ru>
2021-06-11 17:27:42 +0300

    fix: date formatter

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -8,8 +8,8 @@ import java.util.*
 class DateFormatter {
     fun toTextDay(day: String, month: String, year: String): String {
         val formatter = DateTimeFormatter.ofPattern("d MMMM, EEEE", Locale("ru"))
-        val date = LocalDate.of(year.toInt(), month.toInt(), day.toInt())
         return try {
+            val date = LocalDate.of(year.toInt(), month.toInt(), day.toInt())
             date.format(formatter)
         } catch (e: DateTimeException) {
             "Такого дня не существует"
