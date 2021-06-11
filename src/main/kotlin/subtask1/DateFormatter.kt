0de46ef3489f857c740f36a8b54867ded5ea6199
JGMax <maksus20.05@mail.ru>
2021-06-11 16:45:55 +0300

    feat: add solution

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,26 @@
 package subtask1
 
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
 class DateFormatter {
 
-    // TODO: Complete the following function
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+        val toDateParser = SimpleDateFormat("yyyy-MM-dd", Locale("ru", "RU"))
+        toDateParser.isLenient = false
+        return try {
+            val date = toDateParser.parse("$year-${month.dateFormat()}-${day.dateFormat()}")
+            SimpleDateFormat("d MMMM, EEEE", Locale("ru", "RU")).format(date)
+        } catch (e: ParseException) {
+            "Такого дня не существует"
+        }
     }
+
+    private fun String.dateFormat() =
+        if (toInt() in 0 until 10) {
+            "0$this"
+        } else {
+            this
+        }
 }
