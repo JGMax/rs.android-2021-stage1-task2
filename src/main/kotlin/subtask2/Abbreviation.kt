45f41504d9bdcc71d4fe08a8806e0b5d3d1fd4f8
JGMax <maksus20.05@mail.ru>
2021-06-11 16:46:05 +0300

    feat: add solution

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -2,8 +2,27 @@ package subtask2
 
 class Abbreviation {
 
-    // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        var mutableA = a
+        var i = 0
+        while (i < b.length && i < mutableA.length) {
+            if (mutableA[i] != b[i]) {
+                if (!mutableA[i].isLowerCase()) {
+                    return "NO"
+                }
+                if (mutableA[i].toUpperCase() != b[i]) {
+                    val c = mutableA.slice(0 until i) + mutableA.slice(i + 1 until mutableA.length)
+                    mutableA = c
+                    i--
+                }
+            }
+            i++
+        }
+        return if (mutableA.length >= b.length) {
+            "YES"
+        } else {
+            "NO"
+        }
     }
 }
+
